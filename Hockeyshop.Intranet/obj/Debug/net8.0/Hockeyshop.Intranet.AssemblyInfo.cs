271a39d72c23954b5ad83921cfd742eb53e30999
kris.kensy <kriskensy@proton.me>
2025-05-11 15:44:50 +0200

    Change Intranet: all Edit Views to use _CrudFormContainer

--- Hockeyshop.Intranet/obj/Debug/net8.0/Hockeyshop.Intranet.AssemblyInfo.cs
+++ Hockeyshop.Intranet/obj/Debug/net8.0/Hockeyshop.Intranet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hockeyshop.Intranet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0904d8e89719bebe588941f8d2955565abb92b3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+516291b5bfe860bb37cac3837fc3a6d05d7c4d6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hockeyshop.Intranet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hockeyshop.Intranet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
