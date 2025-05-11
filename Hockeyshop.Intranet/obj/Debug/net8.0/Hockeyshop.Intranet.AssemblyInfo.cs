40c0d619731d7faceded3a819fb7b08c1b4b5bb3
kris.kensy <kriskensy@proton.me>
2025-05-11 10:21:08 +0200

    Change Intranet: all Index Views use _CrudIndexContainer

--- Hockeyshop.Intranet/obj/Debug/net8.0/Hockeyshop.Intranet.AssemblyInfo.cs
+++ Hockeyshop.Intranet/obj/Debug/net8.0/Hockeyshop.Intranet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hockeyshop.Intranet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf791a8732fb5d88ae179ee65e187aa819496b89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f4a210532230579e4d803523f4249da55d82cd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hockeyshop.Intranet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hockeyshop.Intranet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
